a7a88e8e46a8d70879b65b9a5a24d81b24694f33
Ne0shka <neosha.73@gmail.com>
2025-05-08 22:07:19 +0400

    Обновление команды на пинг

--- app/handlers/ping.py
+++ app/handlers/ping.py
@@ -4,7 +4,14 @@ from telethon import events
 
 
 async def init(bot):
-    @bot.on(events.NewMessage(pattern="![пП]инг"))
+    @bot.on(events.NewMessage(pattern="(?i)!? ?ping"))
+    async def handler(e):
+        s = time.time()
+        m = await e.reply("✅ Pong!")
+        d = time.time() - s
+        await m.edit(f"✅ Pong!\n⌛ {d:.2f}s")
+
+    @bot.on(events.NewMessage(pattern="!? ?[пП][иИ][нН][гГ]"))
     async def handler(e):
         s = time.time()
         m = await e.reply("✅ Понг!")
