6721b1a7bea93a7f46e1b44c3d37c092adf2b912
Ne0shka <neosha.73@gmail.com>
2025-05-09 01:28:36 +0400

    Небольшой откат с названиями переменных

--- app/handlers/ping.py
+++ app/handlers/ping.py
@@ -7,9 +7,9 @@ from telethon import events
 async def init(bot):
     @bot.on(events.NewMessage(pattern="(?i)!? ?ping"))
     @bot.on(events.NewMessage(pattern="!? ?[пП][иИ][нН][гГ]"))
-    async def handler(msg):
+    async def handler(event):
         snapshot = time.time()
-        reply = await msg.reply("✅ Понг!")
+        reply = await event.reply("✅ Понг!")
         delta = time.time() - snapshot
         await reply.edit(f"✅ Понг!\n⌛ {delta:.2f} сек.")
         logger.info(f"Пинг: {delta:.2f} сек.")
