682799fe22c3f6878e9b52c353631e6ddd1e2941
Ne0shka <neosha.73@gmail.com>
2025-05-14 16:48:42 +0400

    Typo fix #2 + regex refactor

--- app/handlers/emotes.py
+++ app/handlers/emotes.py
@@ -31,9 +31,10 @@ async def init(bot):
                 await event.reply(f"{sender} кусает {receiver}", file=await emotes_helper.get_gif("bite"), parse_mode="html")
 
     @bot.on(events.NewMessage(pattern="(?i)!? ?lick"))
-    @bot.on(events.NewMessage(pattern="!? ?[лЛ][иИ][зЗ]([ьЬ]|[нН][уУ]([лЛ]|[тТ][ьЬ]))"))
+    @bot.on(events.NewMessage(pattern="!? ?[лЛ][иИ][зЗ][ьЬ]"))
+    @bot.on(events.NewMessage(pattern="!? ?([оО][бБ])?[лЛ][иИ][зЗ][нН][уУ]([лЛ]|[тТ][ьЬ])"))
     async def handler(event):
         if event.is_group:
             sender, receiver = await emotes_helper.get_targets(event)
             if sender and receiver:
-                await event.reply(f"{sender} кусает {receiver}", file=await emotes_helper.get_gif("lick"), parse_mode="html")
+                await event.reply(f"{sender} лижет {receiver}", file=await emotes_helper.get_gif("lick"), parse_mode="html")
